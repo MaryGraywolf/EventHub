da3eca2766ab8342e4fe42fcd64af7b8fefdad3b
Victor Kauan <victorkauan.dev@gmail.com>
2025-10-30 15:18:56 -0300

    Adjust attendee card component styles

--- frontend/src/components/view-event/attendee-card/index.tsx
+++ frontend/src/components/view-event/attendee-card/index.tsx
@@ -1,3 +1,4 @@
+import PersonIcon from "@mui/icons-material/Person";
 import MailOutlineIcon from "@mui/icons-material/MailOutline";
 import type { TAttendee } from "../../../services/event/entities/attendee.type";
 
@@ -5,11 +6,14 @@ type TProps = { attendee: TAttendee };
 
 export default function AttendeeCard({ attendee }: TProps) {
   return (
-    <article className="px-3.5 py-2.5 border border-neutral-200 rounded-md shadow flex flex-col gap-1">
-      <h4 className="text-sm font-semibold">{attendee.name}</h4>
-      <p className="text-neutral-600 text-sm flex items-center gap-1">
+    <article className="text-neutral-600 font-semibold text-sm px-3.5 py-2.5 border border-neutral-200 rounded-md shadow flex flex-col gap-1">
+      <h4 className="flex items-center gap-2">
+        <PersonIcon fontSize="small" />
+        <span>{attendee.name}</span>
+      </h4>
+      <p className="flex items-center gap-2">
         <MailOutlineIcon fontSize="small" />
-        <span className="font-semibold">{attendee.email}</span>
+        <span>{attendee.email}</span>
       </p>
     </article>
   );
