7fede9bb9916a12bf07fc8f6f8863e6ad837d9fa
Victor Kauan <victorkauan.dev@gmail.com>
2025-10-30 15:42:00 -0300

    Add title in events component

--- frontend/src/components/events/index.tsx
+++ frontend/src/components/events/index.tsx
@@ -30,8 +30,11 @@ export default function Events() {
   }
 
   return (
-    <ul className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
-      {renderEvents(data.events)}
-    </ul>
+    <div className="flex flex-col gap-4">
+      <h2 className="text-2xl font-semibold">Eventos</h2>
+      <ul className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+        {renderEvents(data.events)}
+      </ul>
+    </div>
   );
 }
